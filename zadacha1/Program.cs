2192cc26b7d17a47000b994fa3b756857fe49e9b
Kondrat <yo126@yandex.ru>
2023-02-01 23:25:33 +0300

    zadacha 1

--- zadacha1/Program.cs
+++ zadacha1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8 
+// 918 -> 1
+
+System.Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number < 1000)
+{
+    System.Console.WriteLine($"Вторая цифра введеного числа - {number / 10 % 10}");
+}
+else
+{
+    System.Console.WriteLine("Введено не трехзначно число");
+}
